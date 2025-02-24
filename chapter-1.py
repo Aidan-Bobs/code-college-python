55bd759645ba999ac5a94ece16337054c280a7a4
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-24 13:57:42 +0200

    Create chapter-1.py

---
+++ chapter-1.py
@@ -0,0 +1,33 @@
+myName = "aidan"
+
+sirname = "roberts"
+
+""" print(myName, sirname)
+
+print(myName[0].capitalize()+sirname[0].capitalize()) """
+
+x = 1
+
+y = 2
+
+
+""" print(f"The answer to the question {x}+{y}={x+y}") """
+
+"""whitespace operators"""
+print("not indented")
+print("\tindented")
+
+print("\nbelow is a bucket\n'Bucket'\nDear God...\ntheres more\nNo...")
+
+"""Methods"""
+
+
+
+
+"""Math"""
+print(1*2**7+(14/2 - 2))
+
+one, two, three, four = 1, 2, 3, 4
+
+print(four)
+import this
