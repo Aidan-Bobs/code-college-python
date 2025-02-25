9675041a4548aba44436287c102c9f34ca4b0078
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-25 11:17:28 +0200

    list methods

--- chapter3/chapter-3.py
+++ chapter3/chapter-3.py
@@ -1,5 +1,44 @@
-my_List = [1, 2, 3]
+""" my_List = [1, 2, 3]
 
-print(my_List[0])
+#print(my_List[0])
 
-peers = ["Cadee", "Courtney", "Ethan", "Lesedi", "Lindokuhle", "Marvelous", "Mieke", "Phomello", "Pierre", "Ronny", "Sbahle", "Sibusiso", "Tom", "Ulrich"]
+peers = ["Cadee", 
+         "Courtney", 
+         "Ethan", 
+         "Lesedi", 
+         "Lindokuhle", 
+         "Marvelous", 
+         "Mieke", 
+         "Phomello", 
+         "Pierre", 
+         "Ronny", 
+         "Sbahle", 
+         "Sibusiso", 
+         "Tom", 
+         "Ulrich"
+         ]
+
+print(peers[-1])
+
+my_List.append(4)
+my_List.append(5)
+my_List.insert(4, "aidan")
+
+my_List.pop()
+my_List.pop(2)
+my_List.remove(1)
+print(my_List) """
+
+places = ["germany", "japan", "netherlands", "kenya", "china"]
+
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+print(places.reverse())
+print(places)
+print(places.reverse())
+print(places)
+print(places.sort())
+print(places)
+print(places.sort(reverse=True))
