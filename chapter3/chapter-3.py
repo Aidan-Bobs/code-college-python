40eef40947d2084babd724e7cb66230d375c452d
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-25 10:20:27 +0200

    Lists

---
+++ chapter3/chapter-3.py
@@ -0,0 +1,5 @@
+my_List = [1, 2, 3]
+
+print(my_List[0])
+
+peers = ["Cadee", "Courtney", "Ethan", "Lesedi", "Lindokuhle", "Marvelous", "Mieke", "Phomello", "Pierre", "Ronny", "Sbahle", "Sibusiso", "Tom", "Ulrich"]
