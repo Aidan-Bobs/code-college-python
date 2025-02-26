bf0dfea9085a178670f306b40aa040fd61679e32
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-26 08:51:50 +0200

    chap 4 edits

---
+++ chapter4/chapter_4.py
@@ -0,0 +1,32 @@
+movies = ["kung-foo panda", "Bladerunner 2077", "in time"]
+
+for i in movies:
+    print(f"{i.title()}")
+
+
+numbers = range(20)
+numberlist = []
+
+print(numberlist)
+for i in numbers:
+    numberlist.append(i)
+
+print(numberlist)
+
+print(movies[1:2])
+
+
+phat_Tuple = ("one","two","three","four")
+#^ is imutable and cant be changed
+
+phat_List = [1,2,3,4]
+
+phat_List.reverse()
+for i in phat_Tuple:
+    print(i)
+
+
+
+
+
+#do the try it yourself
