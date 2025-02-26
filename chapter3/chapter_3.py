1656fd83719fecb210f76d74276a883f72d34736
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-26 08:51:42 +0200

    chap3 edit

---
+++ chapter3/chapter_3.py
@@ -0,0 +1,56 @@
+my_List = [1, 2, 3]
+
+#print(my_List[0])
+
+peers = ["Cadee", 
+         "Courtney", 
+         "Ethan", 
+         "Lesedi", 
+         "Lindokuhle", 
+         "Marvelous", 
+         "Mieke", 
+         "Phomello", 
+         "Pierre", 
+         "Ronny", 
+         "Sbahle", 
+         "Sibusiso", 
+         "Tom", 
+         "Ulrich"
+         ]
+
+print(peers[-1])
+
+my_List.append(4)
+my_List.append(5)
+my_List.insert(4, "aidan")
+
+my_List.pop()
+my_List.pop(2)
+my_List.remove(1)
+print(my_List) 
+
+places = ["germany", "japan", "netherlands", "kenya", "china"]
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+places.reverse()
+print(places)
+print(places)
+places.reverse()
+print(places)
+print(places)
+places.sort()
+print(places)
+print(places)
+print(places.sort(reverse=True))
+
+#do the try it yourself
+
+
+
+new_List = [1,2,3,4]
+
+new_List.pop(5)
+print(new_List)
