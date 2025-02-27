24471b01b98266bf8218f3eb8cafe1e8eaa930b8
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-27 09:05:33 +0200

    ch5

---
+++ chapter5/chapter_5.py
@@ -0,0 +1,19 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+#gets an input from the user by askin them their name
+my_name=input("whats your name? ")
+
+#checks if name is Aidan
+if my_name == "Aidan":
+    #if the name is aidan the it prints the bellow sentence
+    print("hello, awesomest guy")
+elif my_name == "David":
+    print("hello, Mr. David")
+else:
+    print(f"Hello, {my_name}")
