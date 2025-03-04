f5ffe71bca0c510181c9e17215904390fd4e93e6
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-04 08:38:16 +0200

    Create electric_car.py

---
+++ chapter9/electric_car.py
@@ -0,0 +1,14 @@
+from car import Car as parent_class
+
+from car_battery import Battery
+
+class ElectricCar(parent_class):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
