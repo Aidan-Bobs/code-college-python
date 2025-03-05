e8396d3c7c108596d6119bc8f1cd7330cbad6773
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-05 09:32:13 +0200

    json ch10

---
+++ chapter10/json_read_write/json_stuff.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import json
+
+numbers = [2, 3, 5, 7, 11, 13]
+
+numbers_json_path = Path("numbers.json")
+""" content_numbers_json = json.dumps(numbers)
+
+numbers_json_path.write_text(content_numbers_json)
+ """
+
+content = numbers_json_path.read_text()
