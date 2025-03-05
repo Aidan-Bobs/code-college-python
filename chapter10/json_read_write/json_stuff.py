99ebdd2a3edeeb354c13f9719775cea3a1254fe5
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-06 00:48:39 +0200

    ch12

--- chapter10/json_read_write/json_stuff.py
+++ chapter10/json_read_write/json_stuff.py
@@ -4,9 +4,7 @@ import json
 numbers = [2, 3, 5, 7, 11, 13]
 
 numbers_json_path = Path("numbers.json")
-""" content_numbers_json = json.dumps(numbers)
+content_numbers_json = json.dumps(numbers)
 
 numbers_json_path.write_text(content_numbers_json)
- """
 
-content = numbers_json_path.read_text()
