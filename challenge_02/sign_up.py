67585dd4022a4004d3cb27904a04c2342ce623ea
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-06 00:47:44 +0200

    challeng02

--- challenge_02/sign_up.py
+++ challenge_02/sign_up.py
@@ -6,7 +6,7 @@ class SignUp:
         self.second_name = input("What is your last name: ").strip().title()
         self.email = input("What is your email: ").strip()
 
-        # Create a dictionary mapping role names to Role objects
+        
         roles = {role.name: role for role in roles_list}
 
         print("\nAvailable Positions:")
