0ec27792cf19480e361c54b0c385da67cf999ef0
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-19 14:36:21 +0200

    topic form

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,5 +1,5 @@
 """Defines URL patterns for learning_logs."""
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views
 
 
@@ -7,4 +7,10 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    #details for each topic
+    path('topics/<int:topic_id>', views.topic, name="topic"),
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
     ]
