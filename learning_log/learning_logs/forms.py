d28a107bef5aac409f21396b189e1a6bfd47fbf3
Aidan-Bobs <aidankaroberts@gmail.com>
2025-03-19 15:09:14 +0200

    Adding entries

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -8,3 +8,9 @@ class TopicForm(forms.ModelForm):
         labels = {"text": ""}
 
 
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
